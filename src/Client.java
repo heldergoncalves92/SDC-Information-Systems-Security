125d4fe6fc45e03bfadac89c3fe13462869f92a9
heldergoncalves92 <heldergoncalves92@gmail.com>
2015-03-02 12:25:27 +0000

    Server Working With RC4

--- src/Client.java
+++ src/Client.java
@@ -4,33 +4,71 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class Client{
 	
-	public static void main(String[] args) throws IOException{
-		
-	/*	String host = args[0];
+	private byte[] key = "HELLO cripto!!".getBytes();
+	private boolean bool = true;
+	
+	public Client(){}
+	
+	public void runClient(){
+		/*	String host = args[0];
 		int port = Integer.parseInt(args[1]);
 		Socket s = new Socket(host, port);
 		*/String msg = "";
 		
-		Socket s = new Socket("localhost", 	6000);
+		Socket s;
+		try {
+			s = new Socket("localhost", 	6000);
 		
-		BufferedReader sockIn = new BufferedReader( new InputStreamReader(System.in));
-		BufferedWriter sockOut = new BufferedWriter( new OutputStreamWriter(s.getOutputStream())); 
-		
-		while(true){
-			msg = sockIn.readLine();
-				
-			sockOut.write(msg);
-			sockOut.flush();
+			BufferedReader sockIn = new BufferedReader( new InputStreamReader(System.in));
+			BufferedWriter sockOut = new BufferedWriter( new OutputStreamWriter(s.getOutputStream()));
 			
-			if(msg.equals("Sair")  || msg.equals("Shutsown")) break; 
+			//SecretKeySpec sks = new SecretKeySpec( key, "RC4");
+			//Cipher c = Cipher.getInstance("RC4");
+			
+			SecretKeySpec sks = new SecretKeySpec( key, "AES");
+			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			c.init(Cipher.ENCRYPT_MODE , sks);
+			
+			CipherOutputStream cos = new CipherOutputStream(s.getOutputStream(), c);
+			int test;
+			
+			while(bool){
 				
+				while((test = System.in.read()) != -1){
+					cos.write((byte)test);
+					cos.flush();
+				}
+				
+				/*
+				msg = sockIn.readLine();
+				sockOut.write(msg);
+				sockOut.flush();
+				if(msg.equals("Sair")  || msg.equals("Shutsown")) break; 
+					*/
+			}
+			s.close();
+		} catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		s.close();
-		
+	}
+	
+	
+	public static void main(String[] args){
+		Client cli = new Client();
+		cli.runClient();
+	
 	}
 	
 	
--- src/ThreadServer.java
+++ src/ThreadServer.java
@@ -3,6 +3,18 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class ThreadServer implements Runnable {
@@ -12,6 +24,62 @@ public class ThreadServer implements Runnable {
 	private Socket socket;
 	private BufferedReader in;
 	private BufferedWriter out;
+	private boolean bool = true;
+	
+	private byte[] key = "HELLO cripto!!".getBytes();
+	private byte[] iv = "HELLO cripto!!".getBytes();
+	private Cipher c;
+	private CipherInputStream cis;
+	
+	public void getCipherByType(String type){
+		try {
+			
+			//Type RC4
+			if(type.equals("RC4")){
+				SecretKeySpec sks = new SecretKeySpec( key, "RC4");
+				this.c = Cipher.getInstance("RC4");
+			}
+			
+			//Type AES/CBC/NoPadding
+			else if(type.equals("AES/CBC/NoPadding")){
+				SecretKeySpec aesKey = new SecretKeySpec( key, "AES");
+				this.c = Cipher.getInstance("AES/CBC/NoPadding");
+				c.init(Cipher.DECRYPT_MODE, aesKey, new IvParameterSpec(iv));
+				 
+			}
+			
+			//Type AES/CBC/PKCS5Padding
+			else if(type.equals("AES/CBC/PKCS5Padding")){
+				SecretKeySpec aesKey = new SecretKeySpec( key, "AES");
+				this.c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+				c.init(Cipher.DECRYPT_MODE, aesKey, new IvParameterSpec(iv));
+			}
+			/*
+			//Type AES/CFB8/PKCS5Padding
+			else if(type.equals("AES/CFB8/PKCS5Padding")){
+				SecretKeySpec sks = new SecretKeySpec( key, "AES");
+				this.c = Cipher.getInstance("AES/CFB8/PKCS5Padding");
+			}
+			
+			//Type AES/CFB8/NoPadding
+			else if(type.equals("AES/CFB8/NoPadding")){
+				SecretKeySpec sks = new SecretKeySpec( key, "AES");
+				this.c = Cipher.getInstance("AES/CFB8/NoPadding");
+			}
+			
+			//Type AES/CFB/NoPadding
+			else if(type.equals("AES/CFB/NoPadding")){
+				SecretKeySpec sks = new SecretKeySpec( key, "AES");
+				this.c = Cipher.getInstance("AES/CFB/NoPadding");
+			}
+			
+			*/
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	
 	
 	public ThreadServer(int num, Socket s, BufferedWriter file, Server server){
@@ -19,8 +87,15 @@ public class ThreadServer implements Runnable {
 		this.id=num;
 		this.socket=s;
 		try{
-		this.in = new BufferedReader( new InputStreamReader(s.getInputStream()));
-		}catch(IOException e){System.out.println("ERROR: Creating inputStream!!\n");}
+			this.in = new BufferedReader( new InputStreamReader(s.getInputStream()));
+			
+			getCipherByType("RC4");
+			
+			this.cis = new CipherInputStream(s.getInputStream(), c);
+		
+		}catch(IOException e){
+			System.out.println("ERROR: Creating inputStream!!\n");
+			}
 		this.out = file;
 	}
 	
@@ -28,13 +103,21 @@ public class ThreadServer implements Runnable {
 		String str="";
 		
 		try{
-			while(true){
+			while(bool){
+				
+				int test;
+                while ((test=cis.read()) != -1) {
+                      System.out.print((char) test);
+                }
+				
+				/*
 				str = in.readLine();
 				if(str.equals("Sair")) break;
 				else if(str.equals("Shutdown")){ server.shutdown(); break;}
 				
 				out.write(id+": "+ str + "\n");
 				out.flush();
+				*/
 			}
 			
 			System.out.println("=["+ id +"]=\n");
